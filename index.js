3a19f07929204dfd6fbf9275a92e633c2716c224
joySarkar8 <sarkarjoy498@gmail.com>
2022-11-27 04:15:50 +0600

    categories api done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -8,6 +9,48 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cors());
+
+
+const uri = "mongodb://localhost:27017";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2lhfrsa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function dbConnect() {
+    try {
+        await client.connect();
+        console.log("Database connected");
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+dbConnect();
+
+const Categories = client.db("resellerCube").collection("categories");
+
+
+// categories get api
+app.get("/categoires", async (req, res) => {
+    try {
+        const query = {};
+        const cursor = Categories.find(query);
+
+
+        const categoires = await cursor.toArray();
+        res.send({
+            success: true,
+            message: "Successfully load",
+            data: categoires,
+        });
+
+    } catch (error) {
+        // console.log(error.name, error.message);
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
 
 app.get('/', async (req, res) => {
     res.send('reseller cube server is successfully running');
